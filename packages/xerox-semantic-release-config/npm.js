bab47d33b0868fb616b0e3a266030d384ce8090f
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-11-18 13:11:51 +0000

    chore: un-comment semantic-release-bot

--- packages/xerox-semantic-release-config/npm.js
+++ packages/xerox-semantic-release-config/npm.js
@@ -18,25 +18,25 @@ module.exports = {
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
-    // [
-    //   'semantic-release-slack-bot',
-    //   {
-    //     notifyOnSuccess: true,
-    //     notifyOnFail: false,
-    //     onSuccessTemplate: {
-    //       attachments: [
-    //         {
-    //           title:
-    //             ':rocket: A new version of <$repo_url|$package_name> was just published.',
-    //           text:
-    //             'Version $npm_package_version, triggered by ' +
-    //             process.env.CIRCLE_USERNAME,
-    //           color: 'good',
-    //           attachment_type: 'default',
-    //         },
-    //       ],
-    //     },
-    //   },
-    // ],
+    [
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: false,
+        onSuccessTemplate: {
+          attachments: [
+            {
+              title:
+                ':rocket: A new version of <$repo_url|$package_name> was just published.',
+              text:
+                'Version $npm_package_version, triggered by ' +
+                process.env.CIRCLE_USERNAME,
+              color: 'good',
+              attachment_type: 'default',
+            },
+          ],
+        },
+      },
+    ],
   ],
 };
