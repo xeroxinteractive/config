72f571ba45421fa169cf57f03cb2da71c8dace62
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-12-30 18:38:21 +0000

    feat(stylelint): create initial config

---
+++ packages/xerox-stylelint-config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['stylelint-config-recommended', 'stylelint-prettier/recommended'],
+  rules: {
+    'color-hex-length': 'long',
+    'max-empty-lines': 3,
+    'no-descending-specificity': null,
+  },
+};
---
+++ packages/xerox-stylelint-config/sass.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: './index.js',
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
+};
