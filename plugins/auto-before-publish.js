d01452883a94db7cc393d3b262d33c3a4b57efd6
tinytim84 <tim.ottewell@xerox.com>
2020-10-14 17:02:12 +0100

    feat: add plugin to check remote path

---
+++ plugins/auto-before-publish.js
@@ -0,0 +1,15 @@
+module.exports = class TestPlugin {
+  constructor() {
+    this.name = 'auto-before-publish';
+  }
+
+  /**
+   * Tap into auto plugin points.
+   * @param {import('@auto-it/core').default} auto
+   */
+  apply(auto) {
+    auto.hooks.beforeShipIt.tap(this.name, function (context) {
+      console.log(auto.remote);
+    });
+  }
+};
