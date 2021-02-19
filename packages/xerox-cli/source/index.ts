1d17c1878b05a9092980c74eb9c61bb5744dca13
AndrewLeedham <AndrewLeedham@Outlook.com>
2021-02-19 18:34:00 +0000

    feat(x-cli): add warning if no config found

--- packages/xerox-cli/source/index.ts
+++ packages/xerox-cli/source/index.ts
@@ -1,9 +1,12 @@
 import findUp from 'find-up';
 import path from 'path';
+import chalk from 'chalk';
 
 const configPath = findUp.sync('x-cli.config.js');
 if (configPath) {
   const {run} = require(configPath);
   process.chdir(path.dirname(configPath));
   run();
+} else {
+  console.warn(chalk.yellow('No Xerox CLI configuration found. Make sure your current working directory is within a participating Xerox project.'));
 }
